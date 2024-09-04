3afc09a1c05984cb78b825d483734df9cce8fff3
tktkaj <tktkaj@soongsil.ac.kr>
2024-09-04 22:29:07 +0900

    chore: react springboot연동

---
+++ src/main/java/com/ss/spring/cabbagereactspringboot/CabbageReactSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ss.spring.cabbagereactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbageReactSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabbageReactSpringbootApplication.class, args);
+    }
+
+}
