e8208adacd9df56adb0219439eb462367d718826
tktkaj <tktkaj@soongsil.ac.kr>
2024-09-04 23:09:43 +0900

    chore: Router 구현

--- src/main/fronted/src/App.js
+++ src/main/fronted/src/App.js
@@ -1,20 +1,32 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
+import Home from "./home/Home";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Product from './product/Product'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-    const [hello, setHello] = useState('');
 
-    useEffect(() => {
-        axios.get('/api/test')
-            .then((res) => {
-                setHello(res.data);
-            })
-    }, []);
     return (
         <div className="App">
-            백엔드 데이터 : {hello}
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                   <Route path="/" element={<Home/>}></Route>
+                   <Route path="/product" element={<Product/>}></Route>
+                </Routes>
+                <Footer />
+
+            </BrowserRouter>
         </div>
     );
 }
 
 export default App;
+
+// const [hello, setHello] = useState('');
+// useEffect(() => {
+//     axios.get('/api/test')
+//         .then((res) => {
+//             setHello(res.data);
+//         })
+// }, []);
